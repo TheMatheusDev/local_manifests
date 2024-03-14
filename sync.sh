d34cd578773a880d31c405559a25cb7765d6e24d
Matheus Araújo <apenasmatheus1@gmail.com>
2024-03-14 14:12:24 -0300

    sync.sh: Fix wrong link again

--- sync.sh
+++ sync.sh
@@ -10,7 +10,7 @@ rm -rf device/qcom/common
 git clone https://github.com/TheMatheusDev/device_qcom_common.git -b fourteen --depth=1 device/qcom/common
 
 rm -rf device/qcom/qssi
-git clone https://github.com/TheMatheusDev/android_device_qcom_qssi.git -b uvite --depth=1 device/qcom/qssi
+git clone https://github.com/AOSPA/android_device_qcom_qssi.git -b uvite --depth=1 device/qcom/qssi
 
 # Additional repositories
 rm -rf device/qcom/wlan
